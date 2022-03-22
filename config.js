d54a220156b3587a99ab859a9e47cdf614eee808
SonVV <62418903+SonVV@users.noreply.github.com>
2022-03-22 11:39:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Gửi cho .....<3",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Đỗ Thị Vân Trang',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
