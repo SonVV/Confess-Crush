b3be3581db826c7616c9f58f50b6f8c44e6bbe42
SonVV <62418903+SonVV@users.noreply.github.com>
2022-11-10 10:46:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,18 @@
 const CONFIG = {
     titleWeb: "Gửi cho .....<3",
     introTitle: 'Huyền Nguyễn',
-    introDesc: `Nóc nhà q, ^^`,
+    introDesc: `Nóc nhà trưa nay ăn gì dóa, em đỡ đau bụng chưa
+    Có quạu hem cho cho nóc nhà xả lên tấm tân bé bỏng này nè ^^`,
     btnIntro: '^^Click_HiHi^^',
-    title: 'Có phim mới của vũ trụ Marvel mới ra 🥰',
-    desc: 'Cho phép anh qua đón em cùng đi xem nhé ^^',
-    btnYes: 'Thích lắm <33',
+    title: 'ahjhj Vợ a thấy cái nì hay hem 🥰',
+    desc: 'Gọi là Ck đê, ưng hem ^^',
+    btnYes: 'Ưng lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Thanh kiu em đã đọc^^, Em nhập lý do vào ô trống phía dưới nhé <3',
+    question: 'Vợ a nhập lý do vào ô trống phía dưới nhé <3',
     btnReply: 'Gửi cho SonVV <3',
     reply: 'Đồng ý thì đồng ý, không đồng ý thì cũng đồng ý <33333333',
-    mess: 'Anh biết mà 🥰. Oki lun em nha ^^',
-    messDesc: 'Tối chủ nhật mình đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'A biết mà 🥰. Oki lun nóc nha ^^',
+    messDesc: 'Tối thực hành gọi nhoa ^^.',
+    btnAccept: 'Dạaaaaaaaa Ck <3',
     messLink: 'https://www.facebook.com/Vu.Van.Son0612' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
